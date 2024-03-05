5e732f26d6b07dca39ea8460c1247f2c7db1343a
Irit41 <81565397+Irit41@users.noreply.github.com>
2024-03-05 12:45:06 +0200

    05/03

---
+++ frontend/src/CloudsWithRain.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './cloudsWithRain.css'
+function CloudsWithRain() {
+  return (
+    <div class="rainy-weather">
+    <div class="cloud-main"></div>
+    <div class="cloud-center"></div>
+    <div class="cloud-left"></div>
+    <div class="droplet droplet1"></div>
+    <div class="droplet droplet2"></div>
+    <div class="droplet droplet3"></div>
+    <div class="droplet droplet4"></div>
+    <div class="droplet droplet5"></div>
+    <div class="droplet droplet6"></div>
+    </div>
+  )
+}
+
+export default CloudsWithRain
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Card from "./Card.jsx";
+import CloudsWithRain from "./CloudsWithRain.jsx";
 //import WeatherCard from "./WeatherCard";
+// import { Card } from "./Card.jsx";
 
 //<App />
 ReactDOM.createRoot(document.getElementById("root")).render(
